c3bb21ca590d9f3b9a5bc08cbcdb1a51ea4ad12f
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2025-02-04 18:20:37 +0300

    updated for half dynamic content

--- bestsellers/src/Books.jsx
+++ bestsellers/src/Books.jsx
@@ -2,38 +2,57 @@ import "./css/Books.css";
 
 export default Books;
 
+const first_book = {
+  author: "Mark Twain",
+  title: "İnsan Nedir?",
+  img: "./images/insan_nedir_1.jpg",
+};
+const second_book = {
+  author: "Friedrich Dürrenmatt",
+  title: "Gözlemcileri Gözlemleyenin Gözlemi",
+  img: "./imgaes/gozlemcileri_gozlemleyenin_gozlemi.jpg",
+};
+
 function Books() {
   return (
     <section className="books">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={first_book.img}
+        title={first_book.title}
+        author={first_book.author}
+      />
+      <Book
+        img={second_book.img}
+        title={second_book.title}
+        author={second_book.author}
+      />
+      <Book
+        img={first_book.img}
+        title={first_book.title}
+        author={first_book.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <Image img={props.img} title={props.title} />
+      <Title title={props.title} />
+      <Author author={props.author} />
     </article>
   );
 };
 
-const Image = () => {
-  return <img src="./images/insan_nedir_1.jpg" alt="İnsan Nedir?" />;
+const Image = (props) => {
+  return <img src={props.img} alt={props.title} />;
 };
 
-const Title = () => {
-  return <h1>İnsan Nedir?</h1>;
+const Title = (props) => {
+  return <h1>{props.title}</h1>;
 };
 
-const Author = () => {
-  return <h4>Mark Twain</h4>;
+const Author = (adam) => {
+  return <h4>{adam.author}</h4>;
 };
