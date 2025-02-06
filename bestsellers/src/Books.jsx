3287eda620dc30ccdc17220e24e1827a877d0634
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2025-02-06 16:03:32 +0300

    updated code

--- bestsellers/src/Books.jsx
+++ bestsellers/src/Books.jsx
@@ -6,41 +6,38 @@ const first_book = {
   author: "Mark Twain",
   title: "İnsan Nedir?",
   img: "./images/insan_nedir_1.jpg",
+  id: 1,
 };
 const second_book = {
   author: "Friedrich Dürrenmatt",
   title: "Gözlemcileri Gözlemleyenin Gözlemi",
   img: "./imgaes/gozlemcileri_gozlemleyenin_gozlemi.jpg",
+  id: 2,
 };
 
+const books = [first_book, second_book];
 function Books() {
   return (
     <section className="books">
-      <Book
+      {/* <Book
         img={first_book.img}
         title={first_book.title}
         author={first_book.author}
-      />
-      <Book
-        img={second_book.img}
-        title={second_book.title}
-        author={second_book.author}
-      />
-      <Book
-        img={first_book.img}
-        title={first_book.title}
-        author={first_book.author}
-      />
+        /> */}
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 }
 
-const Book = (props) => {
+const Book = (book) => {
+  const { img, title, author } = book;
   return (
     <article className="book">
-      <Image img={props.img} title={props.title} />
-      <Title title={props.title} />
-      <Author author={props.author} />
+      <Image img={img} title={title} />
+      <Title title={title} />
+      <Author author={author} />
     </article>
   );
 };
