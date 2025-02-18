927e63fadb4b8c035dc20cbb7650d1e3e3536174
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2025-02-18 18:23:51 +0300

    usestate object completed

--- advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,34 @@
+import { data } from "../../../data";
+import { useState } from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+  const clearAll = () => {
+    setPeople([]);
+    console.log("clear all");
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              delete
+            </button>
+          </div>
+        );
+      })}
+      <button onClick={clearAll} className="btn">
+        clear items
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
--- advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    hobby: "coding",
+  });
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState("coding");
+  const displayPerson = () => {
+    setName("john");
+    setAge(27);
+    setHobby("reading");
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{hobby}</h3>
+      <button className="btn" onClick={displayPerson}>
+        Show the other person
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
