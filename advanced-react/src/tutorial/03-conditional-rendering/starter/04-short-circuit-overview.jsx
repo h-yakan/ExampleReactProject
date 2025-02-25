0cc9ccfd9931fc3afbc22c74534928a113b1fd23
Hüseyin Yakan <140379978+h-yakan@users.noreply.github.com>
2025-02-25 17:32:23 +0300

    updated

--- advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  return <h2>{"ASDASD" || "ASD"}</h2>;
 };
 export default ShortCircuitOverview;
